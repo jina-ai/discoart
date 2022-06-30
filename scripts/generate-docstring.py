ca236a7ecc00bde3194c4de32b60ac99379e21c6
Han Xiao <han.xiao@jina.ai>
2022-07-01 00:23:20 +0200

    chore: first commit

---
+++ scripts/generate-docstring.py
@@ -0,0 +1,43 @@
+import yaml
+from yaml import Loader
+
+with open('../discoart/resources/default.yml') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=Loader)
+with open('../discoart/resources/docstrings.yml') as ymlfile:
+    docs = yaml.load(ymlfile, Loader=Loader)
+
+all_args = ['@overload\ndef create(']
+for k, v in cfg.items():
+    v_type = type(v).__name__
+    if k == 'init_image':
+        v_type = 'str'
+    elif k == 'seed':
+        v_type = 'int'
+    if v_type == 'str' and v:
+        v = f'\'{v}\''
+    if k == 'text_prompts':
+        v_type = 'List[str]'
+    elif k == 'width_height':
+        v_type = 'List[int]'
+    all_args.append(f'{k}: Optional[{v_type}] = {v},')
+all_args.append('):')
+all_args.append('"""')
+all_args.append('Create Disco Diffusion artworks and save the result into a DocumentArray.\n')
+for k, v in docs.items():
+    if v:
+        _v = v.replace("\n", "").strip()
+        all_args.append(f':param {k}: {_v}')
+all_args.append(':return: a DocumentArray object that has `n_batches` Documents')
+all_args.append('"""')
+indent = '    '
+print(f'\n{indent}'.join(all_args))
+
+
+# final_str = f'@overload\n{signature_str}\n{indent}"""{doc_str_title}\n\n{doc_str}{return_str}\n\n{noqa_str}\n{indent}"""'
+# final_code = re.sub(
+#     rf'(# overload_inject_start_{regex_tag or cli_entrypoint}).*(# overload_inject_end_{regex_tag or cli_entrypoint})',
+#     f'\\1\n{final_str}\n{indent}\\2',
+#     open(filepath).read(),
+#     0,
+#     re.DOTALL,
+# )
