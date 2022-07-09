115b103e64746c06df41822e4634417b056cf04c
Han Xiao <han.xiao@jina.ai>
2022-07-09 19:34:00 +0200

    feat: add spell checker

--- discoart/__init__.py
+++ discoart/__init__.py
@@ -185,16 +185,21 @@ def create(**kwargs) -> Optional['DocumentArray']:
         do_run(_args, (model, diffusion, clip_models, secondary_model), device)
     except KeyboardInterrupt:
         pass
+    except Exception:
+        from IPython import display
+
+        display.clear_output(wait=True)
+        raise
     finally:
+        from IPython import display
+
+        display.clear_output(wait=True)
+
         _name = _args.name_docarray
 
         if not os.path.exists(f'{_name}.protobuf.lz4'):
             # not even a single document was created
             return
-
-        from IPython import display
-
-        display.clear_output(wait=True)
 
         from docarray import DocumentArray
 
