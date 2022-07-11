68036cccdd02f911266750467ba6b229b6c9fbb8
Han Xiao <han.xiao@jina.ai>
2022-07-11 17:55:49 +0200

    feat: support customized model config

--- discoart/__init__.py
+++ discoart/__init__.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import warnings
 from types import SimpleNamespace
@@ -194,7 +195,9 @@ def create(**kwargs) -> Optional['DocumentArray']:
 
     timestep_respacing = f'ddim{steps}'
     diffusion_steps = (1000 // steps) * steps if steps < 1000 else steps
-    model_config.update(
+    _new_config = copy.deepcopy(model_config)
+
+    _new_config.update(
         {
             'timestep_respacing': timestep_respacing,
             'diffusion_steps': diffusion_steps,
@@ -202,10 +205,10 @@ def create(**kwargs) -> Optional['DocumentArray']:
     )
 
     if _args.diffusion_model_config:
-        model_config.update(_args.diffusion_model_config)
+        _new_config.update(_args.diffusion_model_config)
 
     model, diffusion = load_diffusion_model(
-        model_config, _args.diffusion_model, device=device
+        _new_config, _args.diffusion_model, device=device
     )
 
     clip_models = load_clip_models(
