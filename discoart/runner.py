a59d19447f90e4acf487c10b7d38d3b7fd410b5b
Han Xiao <han.xiao@jina.ai>
2022-07-18 21:33:00 +0200

    chore: update readme

--- discoart/runner.py
+++ discoart/runner.py
@@ -219,6 +219,7 @@ def do_run(args, models, device) -> 'DocumentArray':
                     image_embeds = (
                         model_stat['clip_model'].encode_image(clip_in).unsqueeze(1)
                     )
+                    print(image_embeds.shape, model_stat['target_embeds'].shape)
                     dists = spherical_dist_loss(
                         image_embeds,
                         model_stat['target_embeds'],
