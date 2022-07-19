00bcb7716ae6aaa66517925efacf0ca3f69b3304
Han Xiao <han.xiao@jina.ai>
2022-07-19 19:21:11 +0200

    feat: add skip and stop events

--- discoart/runner.py
+++ discoart/runner.py
@@ -203,12 +203,11 @@ def do_run(args, models, device, events) -> 'DocumentArray':
             else:
                 my_t = torch.ones([n], device=device, dtype=torch.long) * cur_t
                 scaled_x = resize(x, 0.5)
-                out = resize(
-                    diffusion.p_mean_variance(
-                        model, scaled_x, my_t, clip_denoised=False
-                    ),
-                    out_shape=x.shape,
+                out = diffusion.p_mean_variance(
+                    model, scaled_x, my_t, clip_denoised=False
                 )
+                out['pred_xstart'] = resize(out['pred_xstart'], out_shape=x.shape)
+
                 fac = diffusion.sqrt_one_minus_alphas_cumprod[cur_t]
                 print('my_t', my_t, my_t.shape)
                 print('x', x, x.shape)
