46d1dac3c17a31d7e33f2fd6af6bc851d5112661
Han Xiao <han.xiao@jina.ai>
2022-07-19 19:09:00 +0200

    feat: add skip and stop events

--- discoart/runner.py
+++ discoart/runner.py
@@ -206,7 +206,7 @@ def do_run(args, models, device, events) -> 'DocumentArray':
                 fac = diffusion.sqrt_one_minus_alphas_cumprod[cur_t]
                 print('my_t', my_t, my_t.shape)
                 print('x', x, x.shape)
-                print('out', out, out.shape)
+                print('out', out['pred_xstart'], out['pred_xstart'].shape)
                 print('fac', fac)
                 x_in = out['pred_xstart'] * fac + x * (1 - fac)
                 x_in_grad = torch.zeros_like(x_in)
