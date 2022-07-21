72e064d12092fdae1796da306dad7e94c71245b5
Han Xiao <han.xiao@jina.ai>
2022-07-21 15:45:18 +0200

    feat: move transformer to cpu

--- discoart/helper.py
+++ discoart/helper.py
@@ -242,8 +242,9 @@ def load_clip_models(device, enabled: List[str], clip_models: Dict[str, Any] = {
                 m = clip.load(k1, device=device, jit=False)[0]
             clip_models[k] = m.eval().requires_grad_(False)
             if next(m.transformer.parameters()).is_cuda:
-                for comp in ('positional_embedding', 'transformer', 'token_embedding'):
-                    getattr(m, comp).to('cpu').float()
+                for comp in ('transformer', 'token_embedding'):
+                    _c = getattr(m, comp).to('cpu').float()
+                m.positional_embedding = m.positional_embedding.to('cpu').float()
                 free_memory()
                 logger.debug(f'move {k}.transformer back to CPU')
 
