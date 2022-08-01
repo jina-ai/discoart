1479d16b63c96a2fcdaabfa9536231717703f406
Han Xiao <han.xiao@jina.ai>
2022-08-01 23:08:49 +0200

    chore: test ew scale strategy

--- discoart/runner.py
+++ discoart/runner.py
@@ -293,7 +293,9 @@ def do_run(args, models, device, events) -> 'DocumentArray':
                     cut_losses += cut_loss.detach().item()
 
         x_is_NaN = False
-        if x_in_grad != 0 and not torch.isnan(x_in_grad).any():
+        if isinstance(x_in_grad, int) and x_in_grad == 0:
+            grad = torch.zeros_like(x)
+        elif not torch.isnan(x_in_grad).any():
             grad = -torch.autograd.grad(x_in, x, x_in_grad)[0]
         else:
             x_is_NaN = True
