5c1531603e4784f6f91e9868f651cc4253a0e30c
Han Xiao <han.xiao@jina.ai>
2022-08-04 12:52:28 +0200

    feat: add option for visualizing cuts

--- discoart/runner.py
+++ discoart/runner.py
@@ -295,7 +295,7 @@ def do_run(args, models, device, events) -> 'DocumentArray':
 
                         dists = dists.view(
                             [
-                                scheduler.cut_overview + scheduler.cut_innercut,
+                                cuts.shape[0],
                                 x.shape[0],
                                 -1,
                             ]
