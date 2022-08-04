2a787599797701dc48a8119959b9d76b9b7f7083
Han Xiao <han.xiao@jina.ai>
2022-08-04 21:31:20 +0200

    fix: cutouts generation

--- discoart/nn/make_cutouts.py
+++ discoart/nn/make_cutouts.py
@@ -1,83 +1,8 @@
-import math
-
 import torch
 from resize_right import resize
 from torch import nn
 from torch.nn import functional as F
 from torchvision import transforms as T
-from torchvision.transforms import functional as TF
-
-
-def sinc(x):
-    return torch.where(x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([]))
-
-
-def lanczos(x, a):
-    cond = torch.logical_and(-a < x, x < a)
-    out = torch.where(cond, sinc(x) * sinc(x / a), x.new_zeros([]))
-    return out / out.sum()
-
-
-def ramp(ratio, width):
-    n = math.ceil(width / ratio + 1)
-    out = torch.empty([n])
-    cur = 0
-    for i in range(out.shape[0]):
-        out[i] = cur
-        cur += ratio
-    return torch.cat([-out[1:].flip([0]), out])[1:-1]
-
-
-class MakeCutouts(nn.Module):
-    def __init__(self, cut_size, cutn, skip_augs=False):
-        super().__init__()
-        self.cut_size = cut_size
-        self.cutn = cutn
-        self.skip_augs = skip_augs
-        self.augs = T.Compose(
-            [
-                T.RandomHorizontalFlip(p=0.5),
-                T.Lambda(lambda x: x + torch.randn_like(x) * 0.01),
-                T.RandomAffine(degrees=15, translate=(0.1, 0.1)),
-                T.Lambda(lambda x: x + torch.randn_like(x) * 0.01),
-                T.RandomPerspective(distortion_scale=0.4, p=0.7),
-                T.Lambda(lambda x: x + torch.randn_like(x) * 0.01),
-                T.RandomGrayscale(p=0.15),
-                T.Lambda(lambda x: x + torch.randn_like(x) * 0.01),
-                # T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-            ]
-        )
-
-    def forward(self, input):
-        input = T.Pad(input.shape[2] // 4, fill=0)(input)
-        sideY, sideX = input.shape[2:4]
-        max_size = min(sideX, sideY)
-
-        cutouts = []
-        for ch in range(self.cutn):
-            if ch > self.cutn - self.cutn // 4:
-                cutout = input.clone()
-            else:
-                size = int(
-                    max_size
-                    * torch.zeros(
-                        1,
-                    )
-                    .normal_(mean=0.8, std=0.3)
-                    .clip(float(self.cut_size / max_size), 1.0)
-                )
-                offsetx = torch.randint(0, abs(sideX - size + 1), ())
-                offsety = torch.randint(0, abs(sideY - size + 1), ())
-                cutout = input[:, :, offsety : offsety + size, offsetx : offsetx + size]
-
-            if not self.skip_augs:
-                cutout = self.augs(cutout)
-            cutouts.append(resample(cutout, (self.cut_size, self.cut_size)))
-            del cutout
-
-        cutouts = torch.cat(cutouts, dim=0)
-        return cutouts
-
 
 augment = torch.jit.script(
     torch.nn.Sequential(
@@ -128,7 +53,6 @@ class MakeCutoutsDango(nn.Module):
                 (sideX - max_size) // 2,
                 (sideX - max_size) // 2,
             ),
-            **padargs,
         )
 
         cutout = resize(pad_input, out_shape=output_shape)
@@ -146,28 +70,3 @@ class MakeCutoutsDango(nn.Module):
             if i <= int(self.IC_Grey_P * self.InnerCrop):
                 cutout = gray(cutout)
             yield resize(cutout, out_shape=output_shape)
-
-
-def resample(input, size, align_corners=True):
-    n, c, h, w = input.shape
-    dh, dw = size
-
-    input = input.reshape([n * c, 1, h, w])
-
-    if dh < h:
-        kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
-        pad_h = (kernel_h.shape[0] - 1) // 2
-        input = F.pad(input, (0, 0, pad_h, pad_h), 'reflect')
-        input = F.conv2d(input, kernel_h[None, None, :, None])
-
-    if dw < w:
-        kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
-        pad_w = (kernel_w.shape[0] - 1) // 2
-        input = F.pad(input, (pad_w, pad_w, 0, 0), 'reflect')
-        input = F.conv2d(input, kernel_w[None, None, None, :])
-
-    input = input.reshape([n, c, h, w])
-    return F.interpolate(input, size, mode='bicubic', align_corners=align_corners)
-
-
-padargs = {}
